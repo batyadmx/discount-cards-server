25ce442363604b5e13fbfe39b0490619e0efd1b8
Maksim Tsvetkov <atm.tsvetk@gmail.com>
2022-05-30 03:40:01 +0500

    Fix and updates

--- DiscountCards.Core/Domains/Maps/IMapService.cs
+++ DiscountCards.Core/Domains/Maps/IMapService.cs
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
 using System.Device.Location;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-=======
-﻿using System.Threading.Tasks;
->>>>>>> 7ea2102d49b461fbb19414479d2bf751fe2c2ca3
 using DiscountCards.Core.Domains.ShopLocations;
 
 namespace DiscountCards.Core.Domains.Maps
