41bbca9299a99e563504f7e1216c71c7ce135643
batyadmx <andronovaleksandrptg@yandex.ru>
2022-05-30 23:53:11 +0500

    Fix Latitude and Longitude

--- DiscountCards.Data/MapsServices/YandexMapsAPI.cs
+++ DiscountCards.Data/MapsServices/YandexMapsAPI.cs
@@ -33,7 +33,7 @@ namespace DiscountCards.Data.MapAPI
         public async Task<ShopLocation> GetShopLocation(ShopLocationRequest request)
         {
             var api_key = _configuration["SecretYandexApiKey"];
-            var coords = request.Coordinates;
+            var coords = $"{request.Coordinates.Longitude},{request.Coordinates.Latitude}";
             var shop = request.Shop;
 
             var response = await _httpClient.GetAsync($"https://search-maps.yandex.ru/v1/?text={shop}&apikey={api_key}&lang=ru_RU&ll={coords}&results=1");
@@ -63,7 +63,7 @@ namespace DiscountCards.Data.MapAPI
                                            .ToList()
                                            .Select(je => je.GetDouble())
                                            .ToList();
-                    allShopCoords.Add(new GeoCoordinate(shopCoords[0], shopCoords[1]));
+                    allShopCoords.Add(new GeoCoordinate(shopCoords[1], shopCoords[0]));
                 }
             }
 
@@ -74,9 +74,9 @@ namespace DiscountCards.Data.MapAPI
         {
             var format = System.Globalization.CultureInfo.GetCultureInfo("en-US");
             var api_key = _configuration["SecretYandexApiKey"];
-            var coords = request.Coordinates;
+            var coords = $"{request.Coordinates.Longitude.ToString(format)},{request.Coordinates.Latitude.ToString(format)}";
             var shop = request.Shop;
-            var apiReq = $"https://search-maps.yandex.ru/v1/?text={shop}&apikey={api_key}&spn={spnLat.ToString(format)},{spnLon.ToString(format)}&lang=ru_RU&ll={coords}&results=500";
+            var apiReq = $"https://search-maps.yandex.ru/v1/?text={shop}&apikey={api_key}&spn={spnLat.ToString(format)},{spnLon.ToString(format)}&lang=ru_RU&ll={coords}&results=500&rspn=1";
 
             var resp = await _httpClient.GetAsync(apiReq);
 
